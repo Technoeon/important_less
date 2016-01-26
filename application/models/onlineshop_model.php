a1aaecda18a507b2fc7759675912b2818c77f308
Rubel <Rubel@DESKTOP-514RO6O>
2016-01-26 23:39:50 +0600

    Rubel new project add file add controller

---
+++ application/models/onlineshop_model.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of onlineshop_model
+ *
+ * @author Rubel
+ */
+class onlineshop_model {
+    //put your code here
+}
