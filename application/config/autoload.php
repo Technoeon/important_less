e50eab01ce634bb9d0e942e43a111a4b882de95a
Foyez <Foyez@Foyez-PC>
2016-01-28 00:23:19 +0600

    Commit from Foyez Ahamed 1/28/16 update submit

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -109,7 +109,7 @@ $autoload['language'] = array();
 |
 */
 
-$autoload['model'] = array('admin_model');
+$autoload['model'] = array('admin_model','onlineshop_model','te_order_model','te_product_model');
 
 
 /* End of file autoload.php */
--- application/models/admin_model.php
+++ application/models/admin_model.php
@@ -1,7 +1,16 @@
 <?php
 
 class Admin_Model extends CI_Model{
-    //put your code here
+    public function check_login_info($email,$password){
+        $encript = md5($password);
+        $this->db->select('*');
+        $this->db->where('management_email',$email);
+        $this->db->where('managemant_password',$encript);
+        $this->db->where('status',1);
+        $this->db->from('tbl_management');
+        $result = $this->db->get();
+        return $result->row();
+    }
 }
 
 
